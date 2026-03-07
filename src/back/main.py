86aea87c4ab7c09caee057d568a7cc32db823e85
Edwin Ravelo Jr. <edwinravelojr@gmail.com>
2026-03-06 23:15:54 -0800

    Basic register and login in api

--- src/back/main.py
+++ src/back/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"message": "hello world"}
+def root():
+    return {"message": "AztecMatch API is peak"}
+
+class User(BaseModel):
+    email: str
+    password: str
+
+@app.post("/register")
+def register(user: User):
+    return {"message": f"User {user.email} is registered!"}
+
+@app.post("/login")
+def login(user: User):
+    return {"message": f"User {user.email} is logged in!"}
